6dd80fb46de5f90cbd90f593a46854f9c3266377
cardaropolim <m.cardaropoli@live.it>
2024-10-04 10:21:39 +0200

    ajout de la carte

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,19 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+ 
 export default defineConfig({
+  server: {
+    hmr: {
+      overlay: false,
+    },
+    aaaaproxy: {
+      '/api': {
+        target: 'https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/les-arbres/records?limit=50',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
   plugins: [react()],
-})
+});
